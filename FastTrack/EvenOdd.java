2d41a4d4ea4f75a1873d9eec6ae17ba2e96fd59c
Usha1913 <168458997+Usha1913@users.noreply.github.com>
2024-07-15 22:56:52 +0530

    Add files via upload

---
+++ FastTrack/EvenOdd.java
@@ -0,0 +1,27 @@
+package com.day1;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+	
+	           Scanner scanner = new Scanner(System.in);
+	           System.out.print("Enter a number: ");
+	           int num = scanner.nextInt();
+
+	           if (num % 2 == 0) {
+	               System.out.println(num + " is even.");
+	           } else {
+	               System.out.println(num + " is odd.");
+	           }
+	       }
+	   
+	   
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+
---
+++ FastTrack/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+
+		// TODO Auto-generated method stub
+
+	}
+
+
